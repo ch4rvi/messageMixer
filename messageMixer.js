8ef0d14a057385009ce242f098b2a96ea19984b9
Martin Charvát <charvi.martin@gmail.com>
2023-09-02 12:32:48 +0200

    number randomizer fix, add title

--- messageMixer.js
+++ messageMixer.js
@@ -1,9 +1,21 @@
 let names = ["Abigail", "Adam", "Ada", "Adolf", "Adéla", "Adrián", "Adela", "Adrian", "Adina", "Aladár"];
 let format = ["science paper", "tabloid", "article", "notes", "diary", "scripture", "textbook", "essay", "novel"];
 let actions = ["reads", "writes", "translates", "examines", "analyses", "rewrites", "describes", "owns", "preaches", "copies"];
+let titles = [
+"Forecasters call for weather on Monday",
+"Amphibious pitcher makes debut",
+"Cows lose their jobs as milk prices drop",
+"Miracle cure kills fifth patient",
+"Man Accused of Killing Lawyer Receives a New Attorney",
+"State population to double by 2040, babies to blame",
+"Missippi's literacy program shows improvement",
+"Breathing oxygen linked to staying alive",
+"Police arrest everyone on February 22nd",
+"Thursday is cancelled"
+]
 
 function messageMixer() {
-    let randomNumber = Math.floor(Math.random()) * 10;
+    let randomNumber = Math.floor(Math.random() * 100);
 
     let position = 0;
     if (randomNumber >= 0 && randomNumber <= 10) { position = 0; }
@@ -17,7 +29,7 @@ function messageMixer() {
     if (randomNumber > 80 && randomNumber <= 90) { position = 8; }
     if (randomNumber > 90 && randomNumber <= 100) { position = 9; }
 
-    return names[position] + " " + actions[position] + " " + format[position];
+    return names[position] + " " + actions[position] + " " + format[position] + " titled " + titles[position];
     }
 
     console.log(messageMixer());
